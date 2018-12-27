4831617ae76b1db7c049838b73fa175c37a40ab1
vadim <vadim.chumasov@gmail.com>
2018-12-27 21:36:07 +0200

    br5 commit

--- GitTrainer/src/Test.java
+++ GitTrainer/src/Test.java
@@ -4,5 +4,6 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("first commit, cherrypick conflict");
 		System.out.println("force push");
+		System.out.println("branch5 commit");
 	}
 }
