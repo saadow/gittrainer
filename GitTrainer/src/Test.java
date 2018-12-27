60c84f7b48754ef41fdb5af2de75b6eaed8133e7
vadim <vadim.chumasov@gmail.com>
2018-12-27 19:57:42 +0200

    branch1 commit
    Conflicts:
    	GitTrainer/src/Test.java

--- GitTrainer/src/Test.java
+++ GitTrainer/src/Test.java
@@ -2,6 +2,6 @@
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("first commit, changes in branch2");
+		System.out.println("first commit, cherrypick conflict");
 	}
 }
